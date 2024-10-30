1801531665e400a720475ffb1ca3ab11747ceca3
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-10-31 01:14:43 +0300

    start from some basics

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,11 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "reactchatapp-2af36.firebaseapp.com",
   projectId: "reactchatapp-2af36",
   storageBucket: "reactchatapp-2af36.appspot.com",
   messagingSenderId: "274715748752",
-  appId: "1:274715748752:web:a6d78aae8391ff40092b15"
+  appId: "1:274715748752:web:a6d78aae8391ff40092b15",
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
