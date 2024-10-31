e41a66ba17684a46f8042d540227cc31f62b368b
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-10-31 21:01:20 +0300

    handle firebase functionality

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./adduser.css";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../../../../lib/firebase";
+import avatarImg from "../../../../../public/avatar.png";
+
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("name", "==", username));
+      const qSnapShot = await getDocs(q);
+      if (!qSnapShot.empty) {
+        setUser(qSnapShot.docs[0].data());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAddUser = async () => {
+    const chatRef = collection(db, "chats");
+    const userChatRef = collection(db, "userChats");
+    try {
+      const newChatRef = doc(chatRef);
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        message: [],
+      });
+      console.log(newChatRef.id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="addUser">
-      <form action="">
+      <form action="" onSubmit={handleSearch}>
         <input type="text" placeholder="username" name="username" />
         <button>search</button>
       </form>
-      <div className="user">
-        <div className="detail">
-          <img src="" alt="" />
-          <span>sayed rady</span>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || avatarImg} alt="" />
+            <span>sayed rady</span>
+          </div>
+          <button onClick={handleAddUser}>add user</button>
         </div>
-        <button>add user</button>
-      </div>
+      )}
     </div>
   );
 };
